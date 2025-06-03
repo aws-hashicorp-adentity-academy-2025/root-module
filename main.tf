f58d18010d1091d0604f4f4918d61289c0841f9a
Great-Stone <hahohh@gmail.com>
2025-06-03 16:28:36 +0900

    Terraform 구성 업데이트: random_string 리소스 추가 및 IAM 모듈 버전 업데이트

--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
+resource "random_string" "random" {
+  length           = 6
+  special          = false
+}
+
 module "iam-admin" {
   source  = "aws-hashicorp-adentity-academy-2025/iam-admin/aws"
-  version = "0.1.1"
+  version = "0.1.3"
+
+  postfix = random_string.random.result
 }
 
 module "iam-s3" {
   source  = "aws-hashicorp-adentity-academy-2025/iam-s3/aws"
-  version = "0.1.1"
+  version = "0.1.2"
+
+  postfix = random_string.random.result
 }
